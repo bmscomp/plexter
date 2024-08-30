66da4d14b9e219a7a3648deff83e2e21c5be8089
Said BOUDJELDA <bmscomp@gmail.com>
2024-08-30 12:31:07 +0200

    first release

---
+++ src/main/java/com/bmscomp/plexter/PlexApplication.java
@@ -0,0 +1,13 @@
+package com.bmscomp.plexter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmscomp/plexter/PlexterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmscomp.plexter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlexterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
